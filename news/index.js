1cf8e1f9f6514b4f21db1e92f1da8d4c5b709b49
CHENG PENG <15045876+paulpengtw@users.noreply.github.com>
2026-03-25 16:43:37 +0800

    fix news item display

--- news/index.js
+++ news/index.js
@@ -1,5 +1,4 @@
 import openRegister from './2026-02-10-open-register.js';
 import pastEventsMarch25 from './2026-03-25-past-events.js';
 
-export const items = [pastEventsMarch25];
-export const items = [openRegister];
+export const items = [pastEventsMarch25, openRegister];
