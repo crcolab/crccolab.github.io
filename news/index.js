453e0aad49d5498883707d6b4cbae22738754617
CHENG PENG(paulpengtw) <15045876+paulpengtw@users.noreply.github.com>
2026-05-02 18:40:40 +0800

    style: update sponsor card logo dimensions and add overflow handling; add upcoming hackathon event details

--- news/index.js
+++ news/index.js
@@ -1,4 +1,5 @@
 import openRegister from './2026-02-10-open-register.js';
 import pastEventsMarch25 from './2026-03-25-past-events.js';
+import hackathon2026Upcoming from './2026-05-02-hackathon-2026-upcoming.js';
 
-export const items = [pastEventsMarch25, openRegister];
+export const items = [pastEventsMarch25, openRegister, hackathon2026Upcoming];
