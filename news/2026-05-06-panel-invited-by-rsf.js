6e828ac884dd4a44b9dda8981057c31960d8bec7
CHENG PENG(paulpengtw) <15045876+paulpengtw@users.noreply.github.com>
2026-05-06 22:23:01 +0800

    style: add panel event details for May 6, 2026

---
+++ news/2026-05-06-panel-invited-by-rsf.js
@@ -0,0 +1,15 @@
+export default {
+  date: "2026-MAY-06",
+  category: "EVENT",
+  title: "Slides of Undersea Cable Issue and Introduction of Meshtastic Taiwan (invitation by RSF) ",
+  summary: `「簡報連結」無國界記者邀請分享海纜議題與 Meshtastic 社群`,
+  detail: `
+
+Thanks to Reporters Without Borders (RSF) for inviting us to share about the undersea cable issue and the Meshtastic Taiwan community, the following are the slide decks of the presentation:
+
+>>> CHENG PENG's Slide Decks： https://paulpengtw.github.io/crccolab-May-6-2026-why-cable-cuts-matters/ (Licsense: CC BY-SA 4.0)
+>>> Sean Ching's Slide Decks： https://docs.google.com/presentation/d/12XXii10OHNy3xO8sI9PzjoscqG5FojyzFT7FaBgDScc/edit (Licsense: Beerware)
+
+  `,
+  image: "",
+};
--- news/index.js
+++ news/index.js
@@ -1,5 +1,6 @@
 import openRegister from './2026-02-10-open-register.js';
 import pastEventsMarch25 from './2026-03-25-past-events.js';
 import hackathon2026Upcoming from './2026-05-02-hackathon-2026-upcoming.js';
+import may2026panelInvitedByRsf from './2026-05-06-panel-invited-by-rsf.js';
 
-export const items = [hackathon2026Upcoming, pastEventsMarch25, openRegister];
+export const items = [may2026panelInvitedByRsf, hackathon2026Upcoming, pastEventsMarch25, openRegister];
