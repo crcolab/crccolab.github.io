7647d381b92b06c73069f6a5250fb99f9d77b36f
CHENG PENG(paulpengtw) <15045876+paulpengtw@users.noreply.github.com>
2026-05-27 19:02:17 +0800

    style: add after-hackathon event details and update items export order

---
+++ news/2026-05-24-after-hackathon.js
@@ -0,0 +1,21 @@
+export default {
+  date: "2026-MAY-24",
+  category: "EVENT",
+  title: "Slide Decks of Our Panel Discussion (g0v Summit 2026) ",
+  summary: `「簡報連結」數位韌性系列活動 (at) g0v Summit 2026`,
+  detail: `
+
+數位韌性系列活動 (at) g0v Summit 2026 的簡報連結：
+
+>>> 數位生命線：關於網路海纜你該知道的一切 Digital Lifeline: Everything You Need to Know About Subsea Cables
+- 周詳 Sean Chou: https://docs.google.com/presentation/d/1Pj9pSy5K9w1ehX5dsC7cOoDFwmkwbCS2cEdn2K981sc/edit (All rights reserved)
+- 唐若凌 Athena Tong: https://drive.google.com/file/d/1X0F9t83NNC7mw5-82mB0jen2mLDl2EdU/view (All rights reserved)
+- 彭宬 CHENG PENG: https://paulpengtw.github.io/crccolab-implications-of-internet-traffics-drop-to-50-percent-and-what-to-do/ (Licsense: CC BY-SA 4.0)
+
+>>> 斷網之後：打造烏克蘭、緬甸與台灣的通訊韌性網絡 After the Blackout: Off-the-grid Communication Solutions in Ukraine, Myanmar and Taiwan
+- Sean Ching: https://docs.google.com/presentation/d/1OhzajvjjwuUDTEJsLs23il_z6JHUp86_YGE6GeM3hGY/edit (Licsense: Beerware)
+- Aidan: https://drive.google.com/file/d/1Kf45_w129YS3GOH7H_hPnFn-o1cOOsyC/view
+- Michael Suantak: https://drive.google.com/file/d/1GSPfOFlLUVFZg3a49uUmzgSovpkLntdc/view
+  `,
+  image: "",
+};
